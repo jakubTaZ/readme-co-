ad38fac9903a7afc7920187f580cf06b58806c2b
Jakub Tazbir <quubaa925@gmail.com>
2026-05-06 11:46:46 +0200

    Dodano docstring do szyfru Cezara

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 # Autor: Jakub Tazbir
-
+# Obsługiwane szyfry: Cezara
 
 
 def caesar_cipher(text, shift):
+    """
+    Szyfruje tekst za pomocą szyfru Cezara.
+
+    Args:
+        text (str): Tekst do zaszyfrowania. Program obsługuje wielkie litery A-Z.
+        shift (int): Liczba określająca przesunięcie liter w alfabecie.
+
+    Returns:
+        str: Zaszyfrowany tekst.
+
+    Raises:
+        ValueError: Jeśli przesunięcie nie jest liczbą całkowitą.
+    """
     result = ""
+
     for char in text:
-        if 'A' <= char <= 'Z':
+        if "A" <= char <= "Z":
             new_code = ord(char) + shift
-            if new_code > ord('Z'):
-                new_code = ord('A') + (new_code - ord('Z') - 1)
+
+            if new_code > ord("Z"):
+                new_code = ord("A") + (new_code - ord("Z") - 1)
+
             result += chr(new_code)
         else:
             result += char
+
     return result
 
 
